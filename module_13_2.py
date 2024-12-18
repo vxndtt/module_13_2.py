641200203420e978c27ce2ed7db0c69e88aad5f3
vxndtt <alenakad16@mail.ru>
2024-12-18 16:25:06 +0500

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '7961462971:AAHBNMb7_3cy64-jMoy8M6Ku91RSm98jqqM'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage = MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот, помогающий твоему здоровьюю.')
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
